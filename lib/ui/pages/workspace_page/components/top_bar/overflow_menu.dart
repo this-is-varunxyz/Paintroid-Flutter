e526ed189d0dd50e8f138d05273b2b4d5f5e1976
Varun Kushwah <varunkushwah500notes@gmail.com>
2026-03-30 03:05:41 +0530

    feat: integrate advanced options dialog into workspace overflow menu

--- lib/ui/pages/workspace_page/components/top_bar/overflow_menu.dart
+++ lib/ui/pages/workspace_page/components/top_bar/overflow_menu.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oxidized/oxidized.dart';
 import 'package:toast/toast.dart';
-
+import 'package:paintroid/ui/shared/dialogs/advanced_settings_dialog.dart';
 import 'package:paintroid/core/database/project_database.dart';
 import 'package:paintroid/core/localization/app_localizations.dart';
 import 'package:paintroid/core/models/database/project.dart';
@@ -21,7 +21,8 @@ enum OverflowMenuOption {
   saveImage,
   saveProject,
   loadImage,
-  newImage;
+  newImage,
+  advancedOptions;
 
   String localizedLabel(BuildContext context) {
     final localizations = AppLocalizations.of(context);
@@ -36,6 +37,8 @@ enum OverflowMenuOption {
         return localizations.newImage;
       case OverflowMenuOption.saveProject:
         return localizations.saveProject;
+      case OverflowMenuOption.advancedOptions: 
+        return 'Advanced Options';
     }
   }
 }
@@ -82,6 +85,9 @@ class _OverflowMenuState extends ConsumerState<OverflowMenu> {
         break;
       case OverflowMenuOption.newImage:
         ioHandler.newImage(context, this);
+        break;
+        case OverflowMenuOption.advancedOptions: 
+        showAdvancedSettingsDialog(context);   
         break;
     }
   }
